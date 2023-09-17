b9db7d0f8ad134ad1166879218340725a99dafeb
equeu <141184555+equeu@users.noreply.github.com>
2023-09-18 00:20:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -461,7 +461,7 @@ wss.on('connection', (socket) => {
   socket.on('message', async (message) => {
     try {
       const jsonData = JSON.parse(message);
-      if('userId' in jsonData)
+      if('x' in jsonData)
       {
         try {
           const { roomId1, userId, x, y } = jsonData;
