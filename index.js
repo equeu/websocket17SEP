72a42d110beec84fbe89d2ba02ec1948ea3b9562
equeu <141184555+equeu@users.noreply.github.com>
2023-09-17 17:17:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const WebSocket = require('ws');
 const http = require('http');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
 
