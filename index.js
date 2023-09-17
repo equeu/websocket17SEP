6b6e120876f1fbe5e095206c3dab4e7eb537610f
equeu <141184555+equeu@users.noreply.github.com>
2023-09-17 17:14:58 +0500

    doneeeee

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const WebSocket = require('ws');
 const http = require('http');
 
 const app = express();
-<<<<<<< HEAD
 const PORT = 3000;
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
@@ -22,23 +21,6 @@ const { connect } = require('./models/user');
 const Notification = require('./models/notifications'); //
 const server = http.createServer(app);
 
-=======
-const PORT = 8080;
-
-app.use(cors({ origin: true, credentials: true }));
-app.use(bodyParser.json());
-
-const { mongoUrl } = require('./dbConnection');
-const RoomModel = require('./models/roomsdata'); // Path to your roomModel.js file
-const Message = require('./models/message');
-const badgeModel = require('./models/badges');
-const User = require('./models/user');
-const userRoutes = require('./routes/userRoutes');
-const { connect } = require('./models/user');
-const Notification = require('./models/notifications'); //
-const server = http.createServer(app);
-
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -53,16 +35,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 app.use(userRoutes);
-<<<<<<< HEAD
-app.get('/checkroute', async (req, res) => {
-  try {
-    const a={t: 'tahir'};
-    res.json(a);
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+
 app.get('/fetchData', async (req, res) => {
   try {
     //console.log('fetch data');
@@ -412,23 +385,8 @@ const p={email: usern , x:215,y:125};
     console.log("Error Creating Room :  "+ e);
   }
 });
-=======
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
 
-app.get('/fetchData', async (req, res) => {
-  try {
-    console.log('fetch data');
-    // Use the Mongoose model to fetch data
-    const documents = await RoomModel.find();
-    console.log(documents);
-    res.json(documents);
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
-<<<<<<< HEAD
 // WebSocket connection handling
 const wss = new WebSocket.Server({ server });
 
@@ -459,55 +417,8 @@ setInterval(() => {
     fetchAndSendUpdates(roomid);
   }
 }, 2000);
-=======
-app.post('/users/:userId/increment-likes', async (req, res) => {
-  const { userId } = req.params; // Get the user's ID from the route parameter
 
-  try {
-    const user = await User.findOneAndUpdate(
-      { email: userId },
-      { $inc: { likes: 1 } }, // Increment the 'likes' field by 1
-      { new: true } // Return the updated user document
-    );
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    return res.status(200).json({ user });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-// Create a new notification
-app.post('/notifications', async (req, res) => {
-  try {
-    const { sender, recipient, message,type } = req.body;
-    const notification = new Notification({ sender, recipient, message,type });
-    await notification.save();
-    res.status(201).json({ message: 'Notification created' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
-
-// Get unread notifications for a user
-app.get('/notifications/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const notifications = await Notification.find({ recipient: userId, read: false }).populate('sender');
-    res.status(200).json({ notifications });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-<<<<<<< HEAD
 wss.on('connection', (socket) => {
   console.log('WebSocket client connected');
   var roomid = '';
@@ -615,17 +526,6 @@ async function addservermessage(msg,rid){
     const session = await mongoose.startSession();
 
     try {
-=======
-// Add message endpoint
-app.post('/addmessageroom', async (req, res) => {
-  try {
-    const { mymessage, roomid } = req.body.reqdata1;
-    const currenttime = moment().tz('Asia/Karachi').format('YYYY-MM-DD HH:mm:ss');
-
-    const session = await mongoose.startSession();
-
-    try {
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
       session.startTransaction();
 
       let room = await Message.findOne({ room_id: roomid }).session(session);
@@ -657,148 +557,21 @@ app.post('/addmessageroom', async (req, res) => {
       await session.commitTransaction();
       session.endSession();
 
-<<<<<<< HEAD
       console.log('Message Inserted! Using Sockets');
       // res.json({ code: 200, message: 'Message inserted successfully' });
    
-=======
-      console.log('Message Inserted!');
-      res.json({ code: 200, message: 'Message inserted successfully' });
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
     } catch (error) {
       await session.abortTransaction();
       session.endSession();
       throw error;
-<<<<<<< HEAD
     }
   } catch (error) {
     console.error('Error Inserting Message!', error);
     
   }
 }
-=======
-    }
-  } catch (error) {
-    console.error('Error Inserting Message!', error);
-    return res.status(500).send('Error Inserting Message!');
-  }
-});
 
 
-
-app.post('/muteuser', async (req, res) => {
-
-  const {u,t}=req.body.mutedata;
-  console.log(u, t);
-  try {
-    // Use a transaction to handle the message insertion
-    const session = await mongoose.startSession();
-    session.startTransaction();
-  
-    const roomx = await RoomModel.findOne(); // Use findOne instead of find
-    if (roomx) {
-      // Modify the muted array
-      roomx.muted.push(u);
-      roomx.muted.push(t);
-  
-      await roomx.save({ session });
-    }
-  
-    await session.commitTransaction();
-    session.endSession();
-  console.log("Muted Sucessfully!");
-  }
-  catch(e)
-  {
-    console.log("error muting "+ e);
-  }
-});
-app.post('/blockuser', async (req, res) => {
-
-  const {u}=req.body.blockdata;
-  // console.log(u);
-  try {
-    // Use a transaction to handle the message insertion
-    const session = await mongoose.startSession();
-    session.startTransaction();
-  
-    const roomx = await RoomModel.findOne(); // Use findOne instead of find
-    if (roomx) {
-      // Modify the muted array
-      roomx.blocked.push(u);
-
-  
-      await roomx.save({ session });
-    }
-  
-    await session.commitTransaction();
-    session.endSession();
-  console.log("User Blocked Sucessfully!");
-  }
-  catch(e)
-  {
-    console.log("error Blocking user:  "+ e);
-  }
-});
-
-// WebSocket connection handling
-const wss = new WebSocket.Server({ server });
-
-const roomDataMap = new Map();
-
-// Define a function to fetch and send updates for a specific room
-async function fetchAndSendUpdates(roomid) {
-  try {
-    const result = await getfromdb(roomid);
-
-    // Get the connected clients for the specific room
-    const clients = roomDataMap.get(roomid) || [];
-
-    // Send the result to all connected clients of this room
-    clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(result));
-      }
-    });
-  } catch (error) {
-    console.error(`Error in background data retrieval for room ${roomid}:`, error);
-  }
-}
-
-// Run the fetchAndSendUpdates function for each room every 3 seconds
-setInterval(() => {
-  for (const roomid of roomDataMap.keys()) {
-    fetchAndSendUpdates(roomid);
-  }
-}, 2000);
-
-
-wss.on('connection', (socket) => {
-  console.log('WebSocket client connected');
-  var roomid = '';
-  var finaldatax;
-  var userid;
-  socket.on('message', async (message) => {
-    try {
-      const jsonData = JSON.parse(message);
-      const roomid = jsonData.roomId;
-
-      // Store the client in the roomDataMap based on the roomid
-      if (!roomDataMap.has(roomid)) {
-        roomDataMap.set(roomid, []);
-      }
-      roomDataMap.get(roomid).push(socket);
-
-      // Fetch initial data for this specific room and send it to the client
-      fetchAndSendUpdates(roomid);
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-    }
-  });
-  
-
-});
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
 
 
 async function getfromdb( r) {
@@ -806,15 +579,6 @@ async function getfromdb( r) {
     const roomid = r;
     // const userid = u;
 
-<<<<<<< HEAD
-
-async function getfromdb( r) {
-  try {
-    const roomid = r;
-    // const userid = u;
-
-=======
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
     const mess = await Message.aggregate([
       {
         $match: { room_id: roomid },
@@ -877,11 +641,7 @@ async function getfromdb( r) {
     ]);
 
     const userdata = userdetails.reduce((result, item) => {
-<<<<<<< HEAD
       const { username, password, email, badge, pic, backgroundPic, bio ,likes,friends,premium} = item;
-=======
-      const { username, password, email, badge, pic, backgroundPic, bio } = item;
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
       result[email] = {
         name: username,
         email: email,
@@ -890,17 +650,13 @@ async function getfromdb( r) {
         pic: pic,
         backgroundPic: backgroundPic,
         bio: bio,
-<<<<<<< HEAD
         likes: likes,
         friends: friends,
         preminum: premium
-=======
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
       };
       return result;
     }, {});
 
-<<<<<<< HEAD
     RoomModel.updateMany({}, { $set: { 'users': useremails } })
     .then(result => {
       // console.log('Users array replaced for:', result.nModified, 'documents');
@@ -930,17 +686,12 @@ async function getfromdb( r) {
 
 
 
-=======
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
     const newData = {
       ...roomdetails[0],
       users: useremails,
     };
-<<<<<<< HEAD
 
 
-=======
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
     
     const finaldata = {
       mess: mess[0],
@@ -986,13 +737,8 @@ app.post('/updateprofilepic', async (req, res) => {
 
   try {
     const { useremail, profileurl } = req.body.imgdata;
-<<<<<<< HEAD
     // console.log(useremail);
     // console.log(profileurl);
-=======
-    console.log(useremail);
-    console.log(profileurl);
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
 
     const user = await User.findOneAndUpdate(
       { email: useremail }, // Search condition
@@ -1014,10 +760,5 @@ app.post('/updateprofilepic', async (req, res) => {
 
 
 server.listen(PORT, () => {
-<<<<<<< HEAD
   console.log('Sockets Server listening on port ' + PORT);
 });
-=======
-  console.log('Server listening on port ' + PORT);
-});
->>>>>>> e0d7747c11f7830e6491598c4dc5e1bbd5ca9521
